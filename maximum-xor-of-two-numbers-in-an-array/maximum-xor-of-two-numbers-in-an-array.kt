6e806470f0686006d354d57f5036f3ee99fca4b7
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-07-20 22:15:11 +0900

    Time: 456 ms (95.83%), Space: 54.6 MB (75.00%) - LeetHub

--- maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.kt
+++ maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.kt
@@ -1,6 +1,10 @@
 class Node(var v: Int=0, var left:Node? = null, var right: Node? = null)
 
-
+/*
+이진트리를 활용한다.
+높은 비트에서부터 한 수 a에 대해서 x비트가 1이면 다른 수 b는 x비트가 0이어야 가장 큰 수가된다.
+만약, x비트가 0인 수가 없다면, x비트가 1인 수 중에서 x+1번 비트를 xor를 했을때 가장 큰 수를 찾으면 되는 방식이다.
+*/
 class Solution {
     fun findMaximumXOR(nums: IntArray): Int {
         var p = Node(0)
