cc5307015e9314acb050d74e291228dfab1fb3f3
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-07-31 15:49:58 +0900

    Update binary-tree-inorder-traversal.kt

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.kt
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.kt
@@ -9,11 +9,24 @@
  * }
  */
 class Solution {
-    var res = ArrayList<Int>()
     fun inorderTraversal(root: TreeNode?): List<Int> {
-        if(root == null) return res
-        inorderTraversal(root.left)
-        res.add(root!!.`val`)
-        return inorderTraversal(root.right)
+        var res = ArrayList<Int>()
+        var stack = Stack<TreeNode>()
+        var cur = root
+
+        while(true) {
+            if(cur != null){
+                stack.add(cur)
+                cur = cur.left
+            }
+            else {
+                if(stack.isEmpty()) break
+                cur = stack.pop()
+                res.add(cur.`val`)
+                cur = cur.right
+            }
+        }
+        
+        return res
     }
 }
