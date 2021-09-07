0119000395912eed2a6f682275ab28c9294e1a02
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-09-08 01:16:37 +0900

    Time: 188 ms (96.48%), Space: 37.1 MB (78.85%) - LeetHub

---
+++ minimum-window-substring/minimum-window-substring.kt
@@ -0,0 +1,42 @@
+class Solution {
+    fun minWindow(s: String, t: String): String {
+        var flag = 0
+        var cnt = IntArray(256)
+        t.forEach {
+            var v = it.toInt()
+            if(cnt[v] == 0) flag++
+            cnt[it.toInt()]++
+        }
+        
+        var j = 0
+        var res = 0x3f3f3f3f
+        var ret = ""
+        
+        for(i in s.indices) {
+            var v = s[i].toInt()
+            cnt[v]--
+            if(cnt[v] == 0) {
+                flag--
+            }
+            
+            if(flag == 0) {
+                if(i-j+1 < res) {
+                    ret = s.substring(j,i+1)
+                    res = i-j+1
+                }
+            }
+            while(j <= i && flag == 0) {
+                if(i-j+1 < res) {
+                    ret = s.substring(j,i+1)
+                    res = i-j+1
+                }
+                var a = s[j].toInt()
+                if(cnt[a] == 0) flag++
+                cnt[a]++
+                ++j
+            }
+        }
+        println(ret)
+        return ret
+    }
+}
