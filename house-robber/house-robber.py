b6da73da36d91eaf4e8c49015b10b430eeea1007
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-12-02 02:21:13 +0900

    Time: 32 ms (70.60%), Space: 14.2 MB (78.81%) - LeetHub

---
+++ house-robber/house-robber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev = 0
+        pprev = 0
+        res = 0
+        for v in nums:
+            pprev, prev = max(pprev, prev), max(prev, pprev+v)
+        return max(pprev,prev)
