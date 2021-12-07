8ef66ccfe1397022877b191a89077e75c29fed60
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-12-08 03:09:09 +0900

    Time: 293 ms (22.24%), Space: 14.4 MB (96.61%) - LeetHub

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -1,25 +1,28 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
+        pa = [-1]*len(isConnected)
+        def get_parent(a):
+            if pa[a] < 0 :return a
+            pa[a] = get_parent(pa[a])
+            return pa[a]
         
-        chk = [0]*n
-        adj = [[] for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                if isConnected[i][j]:
-                    adj[i].append(j)
-                    adj[j].append(i)
+        def join(a,b):
+            p1,p2 = get_parent(a), get_parent(b)
+            if p1 == p2: return
+            
+            if pa[p1] < pa[p2]: p1, p2 = p2,p1
+            pa[p2] += pa[p1]
+            pa[p1] = p2
         
-        def dfs(now):
-            chk[now] = 1
-            for nxt in adj[now]:
-                if chk[nxt] == 0:
-                    dfs(nxt)
-        
+        for i,li in enumerate(isConnected):
+            for j, c in enumerate(li):
+                if c == 1:
+                    join(i,j)
+        chk = set()
         res = 0
-        for i in range(n):
-            if chk[i] == 0:
-                dfs(i)
+        for i in range(len(isConnected)):
+            p = get_parent(i)
+            if p not in chk: 
                 res+=1
-                
+                chk.add(p)
         return res
