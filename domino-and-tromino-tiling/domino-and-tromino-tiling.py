e51dbe1a00735c9066cac78428d5ae8821109c64
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2021-12-13 02:24:49 +0900

    Time: 44 ms (22.44%), Space: 14.3 MB (48.71%) - LeetHub

---
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        dp = [[0]*1001 for _ in range(2)]
+        dp[0][0] = 1
+        dp[1][0] = 1
+        dp[0][1] = 1
+        dp[1][2] = 1
+        M = 1_000_000_000 +7
+        for i in range(2, n+1):
+            dp[0][i] = (dp[0][i-1] + dp[0][i-2]) % M
+            dp[0][i] = (dp[1][i-1]*2 %M + dp[0][i]) %M
+            dp[1][i] = (dp[0][i-2] + dp[1][i-1])%M
+        print(dp[0][:n+1])    
+        print(dp[1][:n+1])    
+        return dp[0][n]
+                
+        
